0a087bc5bc63846ca33ec0b09adf0290892fdd89
JespervnVeen <ninjahamstr@gmail.com>
2024-03-01 23:12:03 +0100

    Update switch.php

--- commands/switch.php
+++ commands/switch.php
@@ -1,6 +1,6 @@
 <?php require_once 'helper_functions.php'; ?>
 <?php
-function switchChar($arguments, $person, $conn) {
+function switchChar($conn) {
     $sql = "SELECT name FROM user WHERE last_played = 'true'";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
@@ -16,29 +16,42 @@ function switchChar($arguments, $person, $conn) {
 
     $allChars = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+    $charList = "";
+
     foreach($allChars as $char) {
         echo $char['name'] . "\n";
+
+        $charList .= $char['name'] . ",";
     }
+
+    $charList = rtrim($charList, ',');
+    $charListArray = explode(",", $charList);
 
     echo"\n";
     echo"Type your new character:\n";
     
     $newChar = readline();
-    echo "\nYou're now playing as " . $newChar;
-    echo"\n";
 
-    foreach($allChars as $char) {
-        if($char['name'] === $newChar) {
+    if(in_array($newChar, $charListArray)) {
+        echo "\nYou're now playing as " . $newChar;
+        echo"\n";
 
-            $sql = "UPDATE user SET last_played = 'false' WHERE last_played = 'true'";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
+        foreach($allChars as $char) {
 
-            $sql = "UPDATE user SET last_played = 'true' WHERE name = :newchar";
-            $stmt = $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':newchar', $newChar);
-            $stmt->execute();
-        }
-    }
+            if($char['name'] === $newChar) {
+
+                $sql = "UPDATE user SET last_played = 'false' WHERE last_played = 'true'";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+    
+                $sql = "UPDATE user SET last_played = 'true' WHERE name = :newchar";
+                $stmt = $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':newchar', $newChar);
+                $stmt->execute();
+            }
+        }    
+    } else {
+        echo "\nThat character is not available\n";
+    }   
 }
 ?>
